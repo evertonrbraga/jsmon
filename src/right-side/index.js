73a1a69d11771a046c6c35d873e02597ce8811c4
Everton Braga <evertonr.braga@gmail.com>
2020-08-11 20:46:09 -0300

    right side primary layout

--- src/right-side/index.js
+++ src/right-side/index.js
@@ -1,9 +1,39 @@
 import React from "react";
 
+import Button from "../components/Button";
 import { Container } from "./styles";
 
-const LeftSide = () => {
-  return <Container>right side</Container>;
+const RightSide = () => {
+  return (
+    <Container>
+      <div className="screen">tech</div>
+      <div className="del-and-put-container">
+        <div className="container">
+          <p>dark mode</p>
+          <div className="btn-container">
+            <Button text="on" />
+            <Button text="off" />
+          </div>
+        </div>
+        <div className="container">
+          <p>edit name</p>
+          <div className="btn-container">
+            <Button text="on" />
+            <Button text="off" />
+          </div>
+        </div>
+      </div>
+      <footer>
+        <Button className="round-btn" colorBtn="#353535" type="round" />
+        <div className="box-container">
+          <div className="box box01">
+            <div className="arrows up-down"></div>
+          </div>
+          <div className="box box02">Hello Everton!</div>
+        </div>
+      </footer>
+    </Container>
+  );
 };
 
-export default LeftSide;
+export default RightSide;
--- src/right-side/styles.js
+++ src/right-side/styles.js
@@ -2,6 +2,60 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 50%;
+  padding: 50px;
   background: #dc0a2d;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   border: 3px solid black;
+
+  .screen {
+    grid-column: 1/3;
+    grid-row: 1/2;
+    height: 150px;
+    border: 3px solid black;
+    padding: 15px;
+    background: #353535;
+  }
+
+  .del-and-put-container {
+    grid-column: 1/3;
+    grid-row: 2/3;
+    display: flex;
+    /* flex-direction: column; */
+
+    .btn-container {
+      display: flex;
+    }
+  }
+
+  footer {
+    grid-column: 1/3;
+    display: flex;
+    flex-direction: column;
+
+    .round-btn {
+      align-self: flex-end;
+    }
+
+    .box-container {
+      display: flex;
+
+      .box {
+        border: 3px solid black;
+      }
+
+      .box01 {
+        width: 50%;
+        /* grid-column: 1/2; */
+        /* grid-row: 3/4; */
+      }
+
+      .box02 {
+        width: 50%;
+        /* grid-column: 2/3; */
+        /* grid-row: 3/4; */
+        background: #51ae5f;
+      }
+    }
+  }
 `;
